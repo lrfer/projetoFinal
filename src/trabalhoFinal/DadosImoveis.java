0c783d6abd77aa3384dc7c48d6bab54370754063
Luiz Fernando Rosa <lr_fernando@hotmail.com>
2021-06-07 09:23:11 -0300

    Refactor telas

--- src/trabalhoFinal/DadosImoveis.java
+++ src/trabalhoFinal/DadosImoveis.java
@@ -4,33 +4,42 @@ import java.util.*;
 
 public class DadosImoveis extends ArquivoBinario<Imoveis> implements IDadosImoveis  {
 	private ArrayList<Imoveis> vetImoveis = new ArrayList<Imoveis>();
+        private ArrayList<Casa> vetCasa = new ArrayList<Casa>();
+        private ArrayList<Apartamento> vetApartamento = new ArrayList<Apartamento>();
+        private ArrayList<Terreno> vetTerreno = new ArrayList<Terreno>();
+        private ArrayList<SalaComercial> vetComercial = new ArrayList<SalaComercial>();
 	
 	@Override
 	public void Cadastrar(Imoveis c) {
 		this.vetImoveis.add(c);
 		System.out.println("Total de imoveis inseridos:");
-		System.out.println(this.vetImoveis.size());
+		System.out.println(this.vetImoveis.size() + this.vetCasa.size()
+                + this.vetApartamento.size() + this.vetComercial.size() + this.vetTerreno.size());
 	}
         
         public void Cadastrar(Casa c){
-            this.vetImoveis.add(c);
+            this.vetCasa.add(c);
                 System.out.println("Total de imoveis inseridos:");
-		System.out.println(this.vetImoveis.size());
+		System.out.println(this.vetImoveis.size() + this.vetCasa.size()
+                + this.vetApartamento.size() + this.vetComercial.size() + this.vetTerreno.size());
         }
           public void Cadastrar(Terreno c){
             this.vetImoveis.add(c);
                 System.out.println("Total de imoveis inseridos:");
-		System.out.println(this.vetImoveis.size());
+		System.out.println(this.vetImoveis.size() + this.vetCasa.size()
+                + this.vetApartamento.size() + this.vetComercial.size() + this.vetTerreno.size());
         }
           public void Cadastrar(SalaComercial c){
             this.vetImoveis.add(c);
                 System.out.println("Total de imoveis inseridos:");
-		System.out.println(this.vetImoveis.size());
+		System.out.println(this.vetImoveis.size() + this.vetCasa.size()
+                + this.vetApartamento.size() + this.vetComercial.size() + this.vetTerreno.size());
         }
             public void Cadastrar(Apartamento c){
-            this.vetImoveis.add(c);
+            this.vetApartamento.add(c);
                 System.out.println("Total de imoveis inseridos:");
-		System.out.println(this.vetImoveis.size());
+		System.out.println(this.vetImoveis.size() + this.vetCasa.size()
+                + this.vetApartamento.size() + this.vetComercial.size() + this.vetTerreno.size());
         }
 	
 	@Override
@@ -54,5 +63,24 @@ public class DadosImoveis extends ArquivoBinario<Imoveis> implements IDadosImove
 		 return c;
 	}
 
+    @Override
+    public ArrayList<Casa> getCasas() {
+        return this.vetCasa;
+    }
+
+    @Override
+    public ArrayList<Apartamento> getApartamentos() {
+        return this.vetApartamento;
+    }
+
+    @Override
+    public ArrayList<SalaComercial> getSalasComercial() {
+        return this.vetComercial;
+    }
+
+    @Override
+    public ArrayList<Terreno> getTerrenos() {
+        return this.vetTerreno;
+    }
 
 }
--- src/trabalhoFinal/IDadosImoveis.java
+++ src/trabalhoFinal/IDadosImoveis.java
@@ -1,5 +1,7 @@
 package trabalhoFinal;
 
+import java.util.ArrayList;
+
 public interface IDadosImoveis {
 
 	void Cadastrar(Imoveis c);
@@ -7,5 +9,14 @@ public interface IDadosImoveis {
 	void Listar();
 
 	Imoveis Buscar(String situacao);
+        
+        ArrayList<Casa> getCasas();
+        
+        ArrayList<Apartamento> getApartamentos();
+        
+        ArrayList<SalaComercial> getSalasComercial();
+        
+        ArrayList<Terreno> getTerrenos();
+        
 
 }
